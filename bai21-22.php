05cbc6d71dff61e9243c9202e96f53cd7b9988a1
khangkhoai <khangnguyen2311998@gmail.com>
2021-03-04 22:18:31 +0700

    hoan thanh bai 22

--- bai21-22.php
+++ bai21-22.php
@@ -1,54 +1,49 @@
 <?php
 
-function calSalary($salary,$n)
-{	
-	for ($i=1; $i<$n ; $i++)
-	{
-		$salary=$salary*1.1;
+function calSalary($salary, $n)
+{
+	for ($i = 1; $i < $n; $i++) {
+		$salary = $salary * 1.1;
 	}
 	return $salary;
 }
 // print_r(calSalary(100,2));
 
-function calSalaryRecursion($salary,$n){
-	if($n<=1)
-	{
+function calSalaryRecursion($salary, $n)
+{
+	if ($n <= 1) {
 		return $salary;
 	}
-	return calSalaryRecursion($salary*1.1,$n-1);	
+	return calSalaryRecursion($salary * 1.1, $n - 1);
 }
 // print_r(calSalaryRecursion(100,2));
 
 function calMonth($money, $rate)
 {
 	$startmoney = $money;
-	$count=0;
-	while($money < $startmoney*2)
-	{
+	$count = 0;
+	while ($money < $startmoney * 2) {
 		$count++;
-		$money = $money*(1+$rate);
-	}	
-	return $count;	
-	
+		$money = $money * (1 + $rate);
+	}
+	return $count;
 }
-print_r(calMonth(100,0.06));
+print_r(calMonth(100, 0.06));
 
 function calMoney($money, $rate, $month)
 {
-	if($month == 0) 
-	{
+	if ($month == 0) {
 		return $money;
 	}
-	return calMoney($money, $rate, $month-1)+ calMoney($money, $rate, $month-1)*($rate/100) ;
+	return calMoney($money, $rate, $month - 1) + calMoney($money, $rate, $month - 1) * ($rate / 100);
 }
 
-function calMonthRecursion($money, $rate){
-	$month=0;
-	while(calMoney($money, $rate, $month) < 2*$money)
-	{	
+function calMonthRecursion($money, $rate)
+{
+	$month = 0;
+	while (calMoney($money, $rate, $month) < 2 * $money) {
 		$month++;
-	}	
+	}
 	return $month;
 }
-print(calMonthRecursion(100,6));
-?>
+print(calMonthRecursion(100, 6));
